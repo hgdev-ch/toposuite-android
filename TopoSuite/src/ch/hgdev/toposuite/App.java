f4b830a14758371ace06f0edd36b42b2500c996b
Robin Hahling <robin.hahling@hgdev.ch>
2014-02-03 13:37:15 +0100

    add a class for handling application settings

---
+++ TopoSuite/src/ch/hgdev/toposuite/App.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package ch.hgdev.toposuite;
+
+import android.app.Application;
+
+/**
+ * Handle every settings that need to be global to the application.
+ * 
+ * @author HGdev
+ * 
+ */
+public class App extends Application {
+
+}
