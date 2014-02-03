4669c72dcef1ef654323ffc43492d586e27ce894
Robin Hahling <robin.hahling@hgdev.ch>
2014-02-03 21:21:45 +0100

    add about information

--- TopoSuite/src/ch/hgdev/toposuite/entry/MainActivity.java
+++ TopoSuite/src/ch/hgdev/toposuite/entry/MainActivity.java
@@ -1,10 +1,19 @@
 package ch.hgdev.toposuite.entry;
 
+import java.util.Calendar;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.webkit.WebView;
 import ch.hgdev.toposuite.R;
 import ch.hgdev.toposuite.TopoSuiteActivity;
+import ch.hgdev.toposuite.utils.Logger;
 
 /**
+ * Starting activity of the application.
  * 
  * @author HGdev
  * 
@@ -14,5 +23,56 @@ public class MainActivity extends TopoSuiteActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_main);
+        ViewGroup layout = (ViewGroup) this.findViewById(R.id.am_main_layout);
+        layout.addView(this.about());
+    }
+
+    /**
+     * Set application about informations such as application name, authors,
+     * copyright and so on.
+     */
+    private WebView about() {
+        String appName = this.getString(R.string.app_name);
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        WebView view = new WebView(this);
+        StringBuilder html = new StringBuilder()
+                .append("<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\" />")
+                // TODO add logo image
+                // .append("<img src=\"file:///android_asset/icon.png\" alt=\"")
+                // .append(appName)
+                // .append("\"/>")
+                .append("<h1>")
+                .append(String.format(this.getString(R.string.about_title), appName))
+                .append("</h1><p>")
+                .append(appName)
+                .append(" ")
+                .append(String.format(this.getString(R.string.app_version), this.getVersionNumber()))
+                .append("</p><p>")
+                .append(this.getString(R.string.developed_by))
+                .append(":<br/>")
+                .append(String.format(this.getString(R.string.app_developer_full_info),
+                        this.getString(R.string.app_developer),
+                        this.getString(R.string.app_developer_webpage_url)))
+                .append("</p>")
+                .append(String.format(this.getString(R.string.app_copyright), year, this.getString(R.string.crag)))
+                .append("</p>");
+        view.loadDataWithBaseURL("file:///android_res/drawable/", html.toString(), "text/html", "utf-8", null);
+        return view;
+    }
+
+    /**
+     * Get current application version number.
+     * 
+     * @return String version of the application.
+     */
+    private String getVersionNumber() {
+        String version = "?";
+        try {
+            PackageInfo pi = this.getPackageManager().getPackageInfo(this.getPackageName(), 0);
+            version = pi.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e(Logger.RESSOURCE_NOT_FOUND, "Application name", e);
+        }
+        return version;
     }
 }
