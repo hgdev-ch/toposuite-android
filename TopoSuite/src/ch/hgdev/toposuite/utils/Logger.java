634deee133201a59f95d581f623ea6ee2b930a13
Robin Hahling <robin.hahling@hgdev.ch>
2014-02-03 21:01:06 +0100

    add logger class

---
+++ TopoSuite/src/ch/hgdev/toposuite/utils/Logger.java
@@ -0,0 +1,11 @@
+package ch.hgdev.toposuite.utils;
+
+/**
+ * Set of TAGS for application logs.
+ * 
+ * @author HGdev
+ * 
+ */
+public class Logger {
+    public final static String RESSOURCE_NOT_FOUND = "RESSOURCE NOT FOUND: ";
+}
