5d5dc68873efcdb4ec847b1ae5776f5fa2f908c7
Kevin Gillieron <kevin.gillieron@hgdev.ch>
2014-02-04 13:54:20 +0100

    add tests for MathUtils

--- TopoSuiteTest/src/ch/hgdev/toposuite/test/utils/TestMathUtils.java
+++ TopoSuiteTest/src/ch/hgdev/toposuite/test/utils/TestMathUtils.java
@@ -21,18 +21,42 @@ public class TestMathUtils extends TestCase {
     }
 
     public void testIsPositive() {
+        double d1 = 0.0;
+        double d2 = -0.0;
+        double d3 = -0.000000000001;
+        double d4 = 42.0;
+        double d5 = 0.00000000001;
 
+        Assert.assertTrue(MathUtils.isPositive(d4));
+        Assert.assertTrue(MathUtils.isPositive(d5));
+
+        Assert.assertFalse(MathUtils.isPositive(d1));
+        Assert.assertFalse(MathUtils.isPositive(d2));
+        Assert.assertFalse(MathUtils.isPositive(d3));
     }
 
     public void testIsNegative() {
+        double d1 = 0.0;
+        double d2 = -0.0;
+        double d3 = -0.00000000000001;
+        double d4 = 1.43433;
+        double d5 = -42.0;
 
+        Assert.assertTrue(MathUtils.isNegative(d3));
+        Assert.assertTrue(MathUtils.isNegative(d5));
+
+        Assert.assertFalse(MathUtils.isNegative(d1));
+        Assert.assertFalse(MathUtils.isNegative(d2));
+        Assert.assertFalse(MathUtils.isNegative(d4));
     }
 
     public void testRadToGrad() {
-
+        Assert.assertEquals("218.89", String.format("%.2f", MathUtils.radToGrad(3.4383)));
+        Assert.assertEquals("79.132", String.format("%.3f", MathUtils.radToGrad(1.243)));
     }
 
     public void testGradToRad() {
-
+        Assert.assertEquals("3.438", String.format("%.3f", MathUtils.gradToRad(218.89)));
+        Assert.assertEquals("1.243", String.format("%.3f", MathUtils.gradToRad(79.132)));
     }
 }
