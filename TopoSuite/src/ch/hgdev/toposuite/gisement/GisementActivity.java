db9191564f66b0cf7890a487eb4133832c703dea
Kevin Gillieron <kevin.gillieron@hgdev.ch>
2014-02-04 16:39:06 +0100

    add GisementActivity

---
+++ TopoSuite/src/ch/hgdev/toposuite/gisement/GisementActivity.java
@@ -0,0 +1,14 @@
+package ch.hgdev.toposuite.gisement;
+
+import android.os.Bundle;
+import ch.hgdev.toposuite.R;
+import ch.hgdev.toposuite.TopoSuiteActivity;
+
+public class GisementActivity extends TopoSuiteActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_gisement);
+    }
+}
