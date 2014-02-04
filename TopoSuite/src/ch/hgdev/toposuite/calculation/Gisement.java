c2b830def63925c30f12a736b4c48359eb68b78f
Robin Hahling <robin.hahling@hgdev.ch>
2014-02-04 08:18:32 +0100

    fix epsilon on gisement and update a few variable names

--- TopoSuite/src/ch/hgdev/toposuite/calculation/Gisement.java
+++ TopoSuite/src/ch/hgdev/toposuite/calculation/Gisement.java
@@ -8,81 +8,79 @@ import ch.hgdev.toposuite.points.Point;
  * @author HGdev
  */
 public class Gisement extends Calculation {
-    private final static double EPSILON = 0.01;
-    
-    private Point origine;
-    private Point orientation;
-    
-    private double gisement;
-    private double distHoriz;
-    private double altitude;
-    private double slope;
+    private final static double EPSILON = 0.0001;
 
-    public Gisement(Point _origine, Point _orientation) {
+    private Point               origin;
+    private Point               orientation;
+
+    private double              gisement;
+    private double              horizDist;
+    private double              altitude;
+    private double              slope;
+
+    public Gisement(Point _origin, Point _orientation) {
         super("Gisement", "");
-        
-        this.origine = _origine;
+
+        this.origin = _origin;
         this.orientation = _orientation;
-        
+
         this.compute();
     }
-    
+
     public final void compute() {
-        double deltaY = this.orientation.getEast() - this.origine.getEast();
-        double deltaX = this.orientation.getNorth() - this.origine.getNorth();
-        
+        double deltaY = this.orientation.getEast() - this.origin.getEast();
+        double deltaX = this.orientation.getNorth() - this.origin.getNorth();
+
         // complement remains 0.0 if deltaY is positive and deltaX is positive
         // and if deltaY is 0.0 and deltaX is positive.
         double complement = 0.0;
-        
+
         // FIXME precision problem
-        if (isPositive(deltaY) && isZero(deltaX)) {
+        if (this.isPositive(deltaY) && this.isZero(deltaX)) {
             complement = 100.0;
-        } else if (isNegative(deltaY) && isZero(deltaX)) {
+        } else if (this.isNegative(deltaY) && this.isZero(deltaX)) {
             complement = 300.0;
-        } else if ((isZero(deltaY) && isNegative(deltaX)) || (isPositive(deltaY) && isNegative(deltaY))
-                || (isNegative(deltaY) && isNegative(deltaX))) {
+        } else if (this.isZero(deltaY) && this.isNegative(deltaX) || this.isPositive(deltaY) && this.isNegative(deltaY)
+                || this.isNegative(deltaY) && this.isNegative(deltaX)) {
             complement = 200.0;
-        } else if (isNegative(deltaY) && isPositive(deltaX)) {
+        } else if (this.isNegative(deltaY) && this.isPositive(deltaX)) {
             complement = 400.0;
         }
-        
 
         // handle division by zero
         double tmp = 0.0;
         if (deltaX > 0.0) {
-            tmp = Math.atan(deltaY/deltaX);
+            tmp = Math.atan(deltaY / deltaX);
         }
         // TODO create a separate helper for converting rad to grad
-        this.gisement = (tmp/Math.PI) * 200 + complement;
-        
+        this.gisement = tmp / Math.PI * 200 + complement;
+
         // TODO create a separate helper for converting grad to rad
-        this.distHoriz = deltaY / ((Math.sin((this.gisement * Math.PI) / 200)));
+        this.horizDist = deltaY / Math.sin(this.gisement * Math.PI / 200);
     }
-    
+
     private boolean isZero(double d) {
-        final double EPSILON = 0.0001; 
-        return d < EPSILON && d > -EPSILON;
+        return d < Gisement.EPSILON && d > -Gisement.EPSILON;
     }
-    
+
     private boolean isPositive(double d) {
-        return d > EPSILON;
+        return d > Gisement.EPSILON;
     }
-    
+
     private boolean isNegative(double d) {
-        return d < EPSILON;
+        return d < Gisement.EPSILON;
     }
 
-    public Point getOrigine() {
-        return origine;
+    public Point getOrigin() {
+        return this.origin;
     }
 
-    public void setOrigine(Point _origine) {
-        this.origine = _origine;
+    public void setOrigin(Point _origin) {
+        this.origin = _origin;
     }
 
     public Point getOrientation() {
-        return orientation;
+        return this.orientation;
     }
 
     public void setOrientation(Point _orientation) {
@@ -90,18 +88,18 @@ public class Gisement extends Calculation {
     }
 
     public double getGisement() {
-        return gisement;
+        return this.gisement;
     }
 
-    public double getDistHoriz() {
-        return distHoriz;
+    public double getHorizDist() {
+        return this.horizDist;
     }
 
     public double getAltitude() {
-        return altitude;
+        return this.altitude;
     }
 
     public double getSlope() {
-        return slope;
+        return this.slope;
     }
 }
