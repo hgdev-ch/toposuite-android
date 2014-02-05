98f030b9dd2920279bf63cb925ede23dec2dbcc9
Kevin Gillieron <kevin.gillieron@hgdev.ch>
2014-02-05 14:53:32 +0100

    add a new utils to convert dp to pixels

--- TopoSuite/src/ch/hgdev/toposuite/utils/DisplayUtils.java
+++ TopoSuite/src/ch/hgdev/toposuite/utils/DisplayUtils.java
@@ -1,5 +1,8 @@
 package ch.hgdev.toposuite.utils;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.content.Context;
 import ch.hgdev.toposuite.App;
 import ch.hgdev.toposuite.R;
@@ -37,5 +40,27 @@ public class DisplayUtils {
      */
     public static String toString(Context context, boolean value) {
         return value ? context.getString(R.string.yes) : context.getString(R.string.no);
+    }
+    
+    /**
+     * Format a date using the global date format defined in {@link App#dateFormat}
+     * @param d
+     *            a date
+     * @return a formatted date
+     */
+    public static String formatDate(Date d) {
+       SimpleDateFormat df = new SimpleDateFormat(App.dateFormat);    
+       return df.format(d);
+    }
+    
+    /**
+     * Convert dp to pixels
+     * @param dp
+     *            the number of dp
+     * @return the number of pixels
+     */
+    public static int dpToPx(Context context, int dp) {
+        float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dp * scale + 0.5f);
     }
 }
