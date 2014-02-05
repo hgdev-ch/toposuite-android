a740b5a2f48c62c2b3f3768e7300dca245a08e13
Kevin Gillieron <kevin.gillieron@hgdev.ch>
2014-02-05 14:53:00 +0100

    prepare history activity for the context menus

--- TopoSuite/src/ch/hgdev/toposuite/history/HistoryActivity.java
+++ TopoSuite/src/ch/hgdev/toposuite/history/HistoryActivity.java
@@ -1,9 +1,13 @@
 package ch.hgdev.toposuite.history;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.Gravity;
+import android.view.MenuInflater;
+import android.view.View;
 import android.widget.TableLayout;
 import android.widget.TableRow;
-import android.widget.TableRow.LayoutParams;
 import android.widget.TextView;
 import ch.hgdev.toposuite.R;
 import ch.hgdev.toposuite.SharedResources;
@@ -24,9 +28,12 @@ public class HistoryActivity extends TopoSuiteActivity {
         this.table = (TableLayout) findViewById(R.id.history_table);
     }
     
-    private int dpToPx(int dp) {
-        float scale = getResources().getDisplayMetrics().density;
-        return (int) (dp * scale + 0.5f);
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.history_table_row_context_menu, menu);
     }
     
     @Override
@@ -37,26 +44,30 @@ public class HistoryActivity extends TopoSuiteActivity {
                 new Gisement("foobar", new Point(1,322.3232,323.323,134.564,true),
                         new Point(1,322.3232,323.323,134.564,true)));
         
+        int pad = DisplayUtils.dpToPx(this, 16);
+        
         for (Calculation c : SharedResources.getCalculationsHistory()) {
             TableRow row = new TableRow(this);
             row.setBaselineAligned(true);
             
             TextView col = new TextView(this);
             col.setText(c.getType());
-            col.setPadding(dpToPx(12), dpToPx(16), dpToPx(12), dpToPx(16));
+            col.setPadding(pad, pad, pad, pad);
             row.addView(col);
             
             col = new TextView(this);
             col.setText(c.getDescription());
-            col.setPadding(dpToPx(12), dpToPx(16), dpToPx(12), dpToPx(16));
+            col.setPadding(pad, pad, pad, pad);
             row.addView(col);
             
             col = new TextView(this);
-            col.setPadding(dpToPx(12), dpToPx(16), dpToPx(12), dpToPx(16));
+            col.setPadding(pad, pad, pad, pad);
+            col.setGravity(Gravity.CENTER);
             col.setText(DisplayUtils.formatDate(c.getLastModification()));
             row.addView(col);
             
             this.table.addView(row);
+            this.registerForContextMenu(row);
         }
     }
 }
