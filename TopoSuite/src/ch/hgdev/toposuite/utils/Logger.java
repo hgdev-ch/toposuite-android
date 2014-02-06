fea6b35c24537393a4207adc17aa219491220a07
Kevin Gillieron <kevin.gillieron@hgdev.ch>
2014-02-06 13:57:47 +0100

    add tags for logging SQL error + a util function for diplaying a point in the logs

--- TopoSuite/src/ch/hgdev/toposuite/utils/Logger.java
+++ TopoSuite/src/ch/hgdev/toposuite/utils/Logger.java
@@ -1,11 +1,28 @@
 package ch.hgdev.toposuite.utils;
 
+import ch.hgdev.toposuite.points.Point;
+
 /**
- * Set of TAGS for application logs.
+ * Set of TAGS for application logs and useful static method for logging.
  * 
  * @author HGdev
  * 
  */
 public class Logger {
-    public final static String TOPOSUITE_RESSOURCE_NOT_FOUND = "RESSOURCE NOT FOUND: ";
+    public final static String TOPOSUITE_RESSOURCE_NOT_FOUND = "TOPOSUITE RESSOURCE NOT FOUND: ";
+    public final static String TOPOSUITE_SQL_ERROR = "TOPOSUITE SQL ERROR";
+    
+    public final static String TOPOSUITE_SQL_SUCCESS = "TOPOSUITE SQL SUCCESS";
+    
+    /**
+     * Format point for logging
+     * @param point
+     *            a Point
+     * @return formatted point
+     */
+    public static String formatPoint(Point point) {
+        return String.format("Point: {No: %d,  E: %f, N: %f, A: %d, BP: %b}",
+                point.getNumber(), point.getEast(), point.getNorth(),
+                point.getAltitude(), point.isBasePoint());
+    }
 }
