f3d7cf61cbb8aab672dd955c0f4a9649768f9d7c
Kevin Gillieron <kevin.gillieron@hgdev.ch>
2014-02-07 17:05:34 +0100

    increment database version number

--- TopoSuite/src/ch/hgdev/toposuite/App.java
+++ TopoSuite/src/ch/hgdev/toposuite/App.java
@@ -3,6 +3,7 @@ package ch.hgdev.toposuite;
 import android.app.Application;
 import android.database.sqlite.SQLiteOpenHelper;
 import ch.hgdev.toposuite.calculation.Calculation;
+import ch.hgdev.toposuite.dao.CalculationsDataSource;
 import ch.hgdev.toposuite.dao.DBHelper;
 import ch.hgdev.toposuite.dao.PointsDataSource;
 import ch.hgdev.toposuite.dao.collections.DAOMapperArrayList;
@@ -19,46 +20,46 @@ public class App extends Application {
     /**
      * Database file name.
      */
-    public static final String DATABASE = "topo_suite.db";
-    
+    public static final String DATABASE         = "topo_suite.db";
+
     /**
      * Database version. This number must be increased whenever the database
      * schema is upgraded in order to trigger the
      * {@link SQLiteOpenHelper#onUpgrade(android.database.sqlite.SQLiteDatabase, int, int)}
      * method.
      */
-    public static final int DATABASE_VERSION = 2;
-    
+    public static final int    DATABASE_VERSION = 4;
+
     /**
      * Number of decimal to display with dealing with numbers.
      */
-    public static String numberOfDecimals = "%.4f";
-    
+    public static String       numberOfDecimals = "%.4f";
+
     /**
      * Date format.
      */
-    public static final String dateFormat = "MM-dd-yyyy HH:mm";
-    
+    public static final String dateFormat       = "MM-dd-yyyy HH:mm";
+
     /**
      * Database helper.
      */
-    public static DBHelper dbHelper;
+    public static DBHelper     dbHelper;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        
+
         App.dbHelper = new DBHelper(this.getApplicationContext());
-        
+
         DAOMapperTreeSet<Point> points = SharedResources.getSetOfPoints();
         points.setNotifyOnChange(false);
         points.addAll(PointsDataSource.getInstance().findAll());
         points.setNotifyOnChange(true);
-        
+
         DAOMapperArrayList<Calculation> calculations =
                 SharedResources.getCalculationsHistory();
         calculations.setNotifyOnChange(false);
-        calculations.addAll(calculations);
+        calculations.addAll(CalculationsDataSource.getInstance().findAll());
         calculations.setNotifyOnChange(true);
     }
 }
