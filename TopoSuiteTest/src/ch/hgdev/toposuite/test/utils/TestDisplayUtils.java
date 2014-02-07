c3c93a1da49162f72c8f96c078508d3fba040539
Kevin Gillieron <kevin.gillieron@hgdev.ch>
2014-02-07 14:59:18 +0100

    fix test

--- TopoSuiteTest/src/ch/hgdev/toposuite/test/utils/TestDisplayUtils.java
+++ TopoSuiteTest/src/ch/hgdev/toposuite/test/utils/TestDisplayUtils.java
@@ -1,5 +1,6 @@
 package ch.hgdev.toposuite.test.utils;
 
+import junit.framework.Assert;
 import junit.framework.TestCase;
 import ch.hgdev.toposuite.utils.DisplayUtils;
 
@@ -16,11 +17,11 @@ public class TestDisplayUtils extends TestCase {
      * {@link ch.hgdev.toposuite.utils.DisplayUtils#toString(double)}.
      */
     public void testToStringDouble() {
-        assertEquals("12.0043", DisplayUtils.toString(12.0043));
-        assertEquals("0.0000", DisplayUtils.toString(0.0));
-        assertEquals("9832.1230", DisplayUtils.toString(9832.1230));
-        assertEquals("-12.0021", DisplayUtils.toString(-12.0021));
-        assertEquals("42.0000", DisplayUtils.toString(42));
+        Assert.assertEquals("12.0043", DisplayUtils.toString(12.0043));
+        Assert.assertEquals("0.0000", DisplayUtils.toString(0.0));
+        Assert.assertEquals("9832.1230", DisplayUtils.toString(9832.1230));
+        Assert.assertEquals("-12.0021", DisplayUtils.toString(-12.0021));
+        Assert.assertEquals("42.0000", DisplayUtils.toString(42.0));
     }
 
     /**
