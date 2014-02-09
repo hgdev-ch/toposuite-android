33efab961dfabdc8c14218faf9157fd4e2fa343d
Kevin Gillieron <kevin.gillieron@hgdev.ch>
2014-02-09 18:08:09 +0100

    add array list of orientations adapter

---
+++ TopoSuite/src/ch/hgdev/toposuite/calculation/activities/abriss/ArrayListOfOrientationsAdapter.java
@@ -0,0 +1,57 @@
+package ch.hgdev.toposuite.calculation.activities.abriss;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import ch.hgdev.toposuite.R;
+import ch.hgdev.toposuite.calculation.Measure;
+import ch.hgdev.toposuite.utils.DisplayUtils;
+
+public class ArrayListOfOrientationsAdapter extends ArrayAdapter<Measure> {
+    private final ArrayList<Measure> orientations;
+
+    public ArrayListOfOrientationsAdapter(Context context, int textViewResourceId,
+            ArrayList<Measure> orientations) {
+        super(context, textViewResourceId, orientations);
+        this.orientations = orientations;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) this.getContext().getSystemService(
+                    Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.orientations_list_item, null);
+        }
+
+        Measure orientation = this.orientations.get(position);
+        if (orientation != null) {
+            TextView numberTextView = (TextView) view.findViewById(R.id.point_number_item);
+            TextView horizOrientTextView = (TextView) view.findViewById(R.id.horiz_orient_item);
+            TextView horizDistTextView = (TextView) view.findViewById(R.id.horiz_dist_item);
+            TextView altitudeTextView = (TextView) view.findViewById(R.id.altitude_item);
+
+            if (numberTextView != null) {
+                numberTextView.setText(DisplayUtils.toString(orientation.getOrientation()
+                        .getNumber()));
+            }
+            if (horizOrientTextView != null) {
+                horizOrientTextView.setText(DisplayUtils.toString(orientation.getHorizDir()));
+            }
+            if (horizDistTextView != null) {
+                horizDistTextView.setText(DisplayUtils.toString(orientation.getDistance()));
+            }
+            if (altitudeTextView != null) {
+                altitudeTextView.setText(DisplayUtils.toString(orientation.getS()));
+            }
+        }
+
+        return view;
+    }
+}
