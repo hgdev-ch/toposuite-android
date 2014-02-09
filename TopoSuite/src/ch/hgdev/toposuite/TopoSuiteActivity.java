cb6686bfda4ba75533c7b49a138fe0c1d88df231
Kevin Gillieron <kevin.gillieron@hgdev.ch>
2014-02-09 16:34:23 +0100

    rename package Abriss into abriss

--- TopoSuite/src/ch/hgdev/toposuite/TopoSuiteActivity.java
+++ TopoSuite/src/ch/hgdev/toposuite/TopoSuiteActivity.java
@@ -13,7 +13,7 @@ import android.view.ViewStub;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import ch.hgdev.toposuite.calculation.activities.Abriss.AbrissActivity;
+import ch.hgdev.toposuite.calculation.activities.abriss.AbrissActivity;
 import ch.hgdev.toposuite.calculation.activities.gisement.GisementActivity;
 import ch.hgdev.toposuite.entry.MainActivity;
 import ch.hgdev.toposuite.history.HistoryActivity;
--- TopoSuite/src/ch/hgdev/toposuite/calculation/activities/abriss/AbrissActivity.java
+++ TopoSuite/src/ch/hgdev/toposuite/calculation/activities/abriss/AbrissActivity.java
@@ -1,4 +1,4 @@
-package ch.hgdev.toposuite.calculation.activities.Abriss;
+package ch.hgdev.toposuite.calculation.activities.abriss;
 
 import java.util.ArrayList;
 import java.util.List;
