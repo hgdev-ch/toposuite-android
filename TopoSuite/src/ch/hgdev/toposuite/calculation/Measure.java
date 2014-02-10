60961ebf6f7322749010a1028e3356aa9b2f478d
Kevin Gillieron <kevin.gillieron@hgdev.ch>
2014-02-10 10:19:09 +0100

    implement the Parcelable interface in the Measure class

--- TopoSuite/src/ch/hgdev/toposuite/calculation/Measure.java
+++ TopoSuite/src/ch/hgdev/toposuite/calculation/Measure.java
@@ -1,15 +1,35 @@
 package ch.hgdev.toposuite.calculation;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+import ch.hgdev.toposuite.SharedResources;
 import ch.hgdev.toposuite.points.Point;
 
-public class Measure {
-    private Point  orientation;
-    private double horizDir;
-    private double zenAngle;
-    private double distance;
-    private double s;
-    private double latDepl;
-    private double lonDepl;
+public class Measure implements Parcelable {
+    private Point                                   orientation;
+    private double                                  horizDir;
+    private double                                  zenAngle;
+    private double                                  distance;
+    private double                                  s;
+    private double                                  latDepl;
+    private double                                  lonDepl;
+
+    /**
+     * Attribute used by the Parcelable interface.
+     */
+    public static final Parcelable.Creator<Measure> CREATOR =
+            new Parcelable.Creator<Measure>() {
+        @Override
+        public Measure createFromParcel(Parcel in) {
+            return new Measure(in);
+        }
+
+        @Override
+        public Measure[] newArray(
+                int size) {
+            return new Measure[size];
+        }
+    };
 
     public Measure(Point _orientation, double _horizDir, double _zenAngle,
             double _distance, double _s, double _latDepl, double _lonDepl) {
@@ -29,6 +49,40 @@ public class Measure {
 
     public Measure(Point _orientation, double _horizDir) {
         this(_orientation, _horizDir, 100.0, 0.0, 0.0);
+    }
+
+    /**
+     * Private constructor for deserializing a parcelabled Measure.
+     * @param in
+     *            Parcel input
+     */
+    private Measure(Parcel in) {
+        this.orientation = SharedResources.getSetOfPoints().find(in.readInt());
+        this.horizDir = in.readDouble();
+        this.zenAngle = in.readDouble();
+        this.distance = in.readDouble();
+        this.s = in.readDouble();
+        this.latDepl = in.readDouble();
+        this.lonDepl = in.readDouble();
+    }
+
+    @Override
+    public void writeToParcel(Parcel out, int flags) {
+        // the point number that will be used for retrieving the point
+        // in the SharedResources.getSetOfPoints()
+        out.writeInt(this.orientation.getNumber());
+
+        out.writeDouble(this.horizDir);
+        out.writeDouble(this.zenAngle);
+        out.writeDouble(this.distance);
+        out.writeDouble(this.s);
+        out.writeDouble(this.latDepl);
+        out.writeDouble(this.lonDepl);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
     }
 
     public Point getOrientation() {
