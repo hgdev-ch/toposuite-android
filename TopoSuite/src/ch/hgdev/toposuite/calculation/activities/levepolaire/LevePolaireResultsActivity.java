cfa1ce6ed97b9ee7ff45a09825541682368cdf90
Robin Hahling <robin.hahling@hgdev.ch>
2014-02-15 17:42:31 +0100

    add possibility to add a point to the list of points from the polar survey results

--- TopoSuite/src/ch/hgdev/toposuite/calculation/activities/levepolaire/LevePolaireResultsActivity.java
+++ TopoSuite/src/ch/hgdev/toposuite/calculation/activities/levepolaire/LevePolaireResultsActivity.java
@@ -7,15 +7,18 @@ import org.json.JSONObject;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
+import android.view.Gravity;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.ListView;
+import android.widget.Toast;
 import ch.hgdev.toposuite.R;
 import ch.hgdev.toposuite.SharedResources;
 import ch.hgdev.toposuite.TopoSuiteActivity;
 import ch.hgdev.toposuite.calculation.LevePolaire;
+import ch.hgdev.toposuite.calculation.LevePolaire.Result;
 import ch.hgdev.toposuite.calculation.Measure;
 import ch.hgdev.toposuite.points.Point;
 
@@ -60,10 +63,37 @@ public class LevePolaireResultsActivity extends TopoSuiteActivity {
         this.displayResults();
     }
 
+    /**
+     * Display the list of resulting points.
+     */
     private void displayResults() {
         this.adapter = new ArrayListOfResultsAdapter(this, R.layout.leve_polaire_results_list_item,
                 this.levePolaire.getResults());
         this.resultsListView.setAdapter(this.adapter);
+    }
+
+    /**
+     * Save a point to the database of points.
+     * 
+     * @param position
+     *            Position of the point in the list of points.
+     * @return True if it was a success, false otherwise.
+     */
+    private boolean savePoint(int position) {
+        Result r = this.adapter.getItem(position);
+        if (SharedResources.getSetOfPoints().find(r.getDeterminationNumber()) == null) {
+            Point point = new Point(
+                    r.getDeterminationNumber(),
+                    r.getEast(),
+                    r.getNorth(),
+                    r.getAltitude(),
+                    false);
+            SharedResources.getSetOfPoints().add(point);
+            return true;
+        } else {
+            // this point already exists
+            return false;
+        }
     }
 
     @Override
@@ -79,7 +109,19 @@ public class LevePolaireResultsActivity extends TopoSuiteActivity {
 
         switch (item.getItemId()) {
         case R.id.save_point:
-            // TODO implement
+            if (this.savePoint(info.position)) {
+                Toast successToast = Toast.makeText(this,
+                        this.getText(R.string.point_add_success),
+                        Toast.LENGTH_SHORT);
+                successToast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                successToast.show();
+            } else {
+                Toast errorToast = Toast.makeText(this,
+                        this.getText(R.string.point_already_exists),
+                        Toast.LENGTH_SHORT);
+                errorToast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                errorToast.show();
+            }
             return true;
         case R.id.delete_point:
             this.adapter.remove(this.adapter.getItem(info.position));
