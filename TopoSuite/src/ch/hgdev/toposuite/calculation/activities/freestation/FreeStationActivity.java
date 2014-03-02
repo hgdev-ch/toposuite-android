3cd0b8997fed03e42b1930cd9f5bace69fc09732
Kevin Gillieron <kevin.gillieron@hgdev.ch>
2014-03-02 19:48:27 +0100

    menu for the free station

--- TopoSuite/src/ch/hgdev/toposuite/calculation/activities/freestation/FreeStationActivity.java
+++ TopoSuite/src/ch/hgdev/toposuite/calculation/activities/freestation/FreeStationActivity.java
@@ -15,7 +15,7 @@ public class FreeStationActivity extends TopoSuiteActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        //this.getMenuInflater().inflate(R.menu.free_station, menu);
+        this.getMenuInflater().inflate(R.menu.free_station, menu);
         return true;
     }
 
