05fa6935f27bbd542f8b1c78e3ec7acc71ed3d59
Kevin Gillieron <kevin.gillieron@hgdev.ch>
2014-03-06 20:53:20 +0100

    add a util function for reading a Double from an EditText

---
+++ TopoSuite/src/ch/hgdev/toposuite/utils/ViewUtils.java
@@ -0,0 +1,23 @@
+package ch.hgdev.toposuite.utils;
+
+import android.widget.EditText;
+
+/**
+ * Useful functions for interacting with View object.
+ * 
+ * @author HGdev
+ */
+public class ViewUtils {
+
+    /**
+     * Convenient function for easily reading a double value from an EditText.
+     * 
+     * @param editText
+     *            An EditText object
+     * @return
+     */
+    public static double readDouble(EditText editText) {
+        return ((editText == null) || (editText.length() > 0)) ? Double.parseDouble(
+                editText.getText().toString()) : 0.0;
+    }
+}
