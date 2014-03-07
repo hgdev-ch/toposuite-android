8614f562269997785e391c8d3a78be6569782011
Kevin Gillieron <kevin.gillieron@hgdev.ch>
2014-03-07 16:28:25 +0100

    add a new util for reading int from edit text

--- TopoSuite/src/ch/hgdev/toposuite/utils/ViewUtils.java
+++ TopoSuite/src/ch/hgdev/toposuite/utils/ViewUtils.java
@@ -14,10 +14,22 @@ public class ViewUtils {
      * 
      * @param editText
      *            An EditText object
-     * @return
+     * @return The value contained in the edit text as double.
      */
     public static double readDouble(EditText editText) {
         return ((editText != null) && (editText.length() > 0)) ? Double.parseDouble(
                 editText.getText().toString()) : 0.0;
+    }
+
+    /**
+     * Convenient function for easily reading a integer value from an EditText.
+     * 
+     * @param editText
+     *            An EditText object
+     * @return The value contained in the edit text as int.
+     */
+    public static int readInt(EditText editText) {
+        return ((editText != null) && (editText.length() > 0)) ? Integer.parseInt(
+                editText.getText().toString()) : 0;
     }
 }
