80ef5fedbc46fe1bc58e9b13ca21c9ce4c436730
Kevin Gillieron <kevin.gillieron@hgdev.ch>
2014-03-09 20:16:27 +0100

    remove useless menu + make the layout for the About activity

--- TopoSuite/src/ch/hgdev/toposuite/settings/AboutActivity.java
+++ TopoSuite/src/ch/hgdev/toposuite/settings/AboutActivity.java
@@ -15,9 +15,7 @@ public class AboutActivity extends TopoSuiteActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        this.getMenuInflater().inflate(R.menu.about, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
