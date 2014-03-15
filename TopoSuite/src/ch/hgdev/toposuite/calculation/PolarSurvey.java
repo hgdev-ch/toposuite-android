7466375a49171443e315bccde9ed1d3bf820df31
Robin Hahling <robin.hahling@hgdev.ch>
2014-03-16 00:50:22 +0100

    update polar survey calculation description after a new computation (close #441)

--- TopoSuite/src/ch/hgdev/toposuite/calculation/PolarSurvey.java
+++ TopoSuite/src/ch/hgdev/toposuite/calculation/PolarSurvey.java
@@ -12,6 +12,7 @@ import ch.hgdev.toposuite.R;
 import ch.hgdev.toposuite.SharedResources;
 import ch.hgdev.toposuite.calculation.activities.polarsurvey.PolarSurveyActivity;
 import ch.hgdev.toposuite.points.Point;
+import ch.hgdev.toposuite.utils.DisplayUtils;
 import ch.hgdev.toposuite.utils.MathUtils;
 
 /**
@@ -115,6 +116,9 @@ public class PolarSurvey extends Calculation {
 
         // update the calculation last modification date
         this.updateLastModification();
+        this.setDescription(this.getCalculationName()
+                + " - " + App.getContext().getString(R.string.station_label) + ": "
+                + DisplayUtils.toStringForTextView(this.getStation().getNumber()));
         this.notifyUpdate(this);
     }
 
