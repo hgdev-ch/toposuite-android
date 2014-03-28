8e710ce198b77017279b50c7d927ab2a37e2fa93
Kevin Gillieron <kevin.gillieron@hgdev.ch>
2014-03-28 10:41:40 +0100

    redirect to the points manager right after saving new points in the polar survey. close #460

--- TopoSuite/src/ch/hgdev/toposuite/calculation/activities/polarsurvey/PolarSurveyResultsActivity.java
+++ TopoSuite/src/ch/hgdev/toposuite/calculation/activities/polarsurvey/PolarSurveyResultsActivity.java
@@ -37,6 +37,7 @@ public class PolarSurveyResultsActivity extends TopoSuiteActivity implements
     private ArrayListOfResultsAdapter adapter;
 
     private int                       saveCounter;
+    private int                       mergeDialogCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -151,8 +152,19 @@ public class PolarSurveyResultsActivity extends TopoSuiteActivity implements
      * already exists in the database, it is simply skipped.
      */
     private void savePoints() {
+        // rester the merge dialog counter
+        this.mergeDialogCounter = 0;
+
         for (int position = 0; position < this.adapter.getCount(); position++) {
             this.savePoint(position);
+        }
+
+        // If the mergeDialogCounter is still 0, it means that no merge dialog
+        // has been popped-up so far. And since the merge dialog callback handles
+        // the redirection to the points manager itself, it is needed to do it
+        // here.
+        if (this.mergeDialogCounter == 0) {
+            ViewUtils.redirectToPointsManagerActivity(this);
         }
     }
 
@@ -175,6 +187,8 @@ public class PolarSurveyResultsActivity extends TopoSuiteActivity implements
             SharedResources.getSetOfPoints().add(point);
         } else {
             // this point already exists
+            ++this.mergeDialogCounter;
+
             MergePointsDialog dialog = new MergePointsDialog();
 
             Bundle args = new Bundle();
