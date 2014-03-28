8fd4d90a61ab259001a29773069c3ff557c3cad2
Kevin Gillieron <kevin.gillieron@hgdev.ch>
2014-03-28 11:02:10 +0100

    redirect to the points manager right after saving new points in the ortho chemin. close #463 and close #425

--- TopoSuite/src/ch/hgdev/toposuite/calculation/activities/cheminortho/CheminementOrthoResultsActivity.java
+++ TopoSuite/src/ch/hgdev/toposuite/calculation/activities/cheminortho/CheminementOrthoResultsActivity.java
@@ -38,6 +38,7 @@ public class CheminementOrthoResultsActivity extends TopoSuiteActivity implement
     private CheminementOrthogonal     cheminOrtho;
 
     private int                       saveCounter;
+    private int                       mergeDialogCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -145,11 +146,21 @@ public class CheminementOrthoResultsActivity extends TopoSuiteActivity implement
      * already exists in the database, it is simply skipped.
      */
     private void savePoints() {
+        // reset merge dialog counter
+        this.mergeDialogCounter = 0;
+
         // This loop stops at this.adapter.getCount()-1 because the last point
-        // is
-        // the extremity and it must not be changed.
+        // is the extremity and it must not be changed.
         for (int position = 0; position < (this.adapter.getCount() - 1); position++) {
             this.savePoint(position);
+        }
+
+        // If the mergeDialogCounter is still 0, it means that no merge dialog
+        // has been popped-up so far. And since the merge dialog callback handles
+        // the redirection to the points manager itself, it is needed to do it
+        // here.
+        if (this.mergeDialogCounter == 0) {
+            ViewUtils.redirectToPointsManagerActivity(this);
         }
     }
 
@@ -173,6 +184,8 @@ public class CheminementOrthoResultsActivity extends TopoSuiteActivity implement
             return true;
         } else {
             // this point already exists
+            ++this.mergeDialogCounter;
+
             MergePointsDialog dialog = new MergePointsDialog();
 
             Bundle args = new Bundle();
