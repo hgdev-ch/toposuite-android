210a9b6c6fc1daee6e7fa7ff669a97580ccfde2d
Robin Hahling <robin.hahling@hgdev.ch>
2014-04-10 13:54:26 +0200

    change comparator for the set of points so that they are sorted in natural order (close #534)

--- TopoSuite/src/ch/hgdev/toposuite/SharedResources.java
+++ TopoSuite/src/ch/hgdev/toposuite/SharedResources.java
@@ -1,7 +1,5 @@
 package ch.hgdev.toposuite;
 
-import java.util.Comparator;
-
 import ch.hgdev.toposuite.calculation.Calculation;
 import ch.hgdev.toposuite.dao.CalculationsDataSource;
 import ch.hgdev.toposuite.dao.PointsDataSource;
@@ -9,6 +7,7 @@ import ch.hgdev.toposuite.dao.collections.DAOMapperArrayList;
 import ch.hgdev.toposuite.dao.collections.DAOMapperTreeSet;
 import ch.hgdev.toposuite.dao.collections.Searcher;
 import ch.hgdev.toposuite.points.Point;
+import ch.hgdev.toposuite.utils.AlphanumComparator;
 
 /**
  * SharedResources provides an easy way to share resources between the
@@ -53,20 +52,14 @@ public class SharedResources {
      */
     public static DAOMapperTreeSet<Point> getSetOfPoints() {
         if (SharedResources.setOfPoints == null) {
-            SharedResources.setOfPoints = new DAOMapperTreeSet<Point>(new Comparator<Point>() {
-                @Override
-                public int compare(Point left, Point right) {
-                    String l = left.getNumber();
-                    String r = right.getNumber();
-                    return l.compareTo(r);
-                }
-            }, new Searcher<Point>() {
-                @Override
-                public boolean isFound(Point currentElement, Object expectedElement) {
-                    String expectedNumber = (String) expectedElement;
-                    return currentElement.getNumber().equals(expectedNumber);
-                }
-            });
+            SharedResources.setOfPoints = new DAOMapperTreeSet<Point>(new AlphanumComparator(),
+                    new Searcher<Point>() {
+                        @Override
+                        public boolean isFound(Point currentElement, Object expectedElement) {
+                            String expectedNumber = (String) expectedElement;
+                            return currentElement.getNumber().equals(expectedNumber);
+                        }
+                    });
             SharedResources.setOfPoints.registerDAO(PointsDataSource.getInstance());
         }
         return SharedResources.setOfPoints;
