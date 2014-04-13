f58432a82c2de665c99559bddbf52d1d871d64a6
Robin Hahling <robin.hahling@hgdev.ch>
2014-04-13 18:34:23 +0200

    test left slider menu with robotium tests

--- TopoSuiteTest/src/ch/hgdev/toposuite/test/entry/MainActivityTest.java
+++ TopoSuiteTest/src/ch/hgdev/toposuite/test/entry/MainActivityTest.java
@@ -1,6 +1,11 @@
 package ch.hgdev.toposuite.test.entry;
 
 import ch.hgdev.toposuite.entry.MainActivity;
+import ch.hgdev.toposuite.help.HelpActivity;
+import ch.hgdev.toposuite.history.HistoryActivity;
+import ch.hgdev.toposuite.jobs.JobsActivity;
+import ch.hgdev.toposuite.points.PointsManagerActivity;
+import ch.hgdev.toposuite.settings.SettingsActivity;
 import ch.hgdev.toposuite.test.testutils.TestActivity;
 
 import com.robotium.solo.Solo;
@@ -17,15 +22,38 @@ public class MainActivityTest extends TestActivity<MainActivity> {
         super(MainActivity.class);
     }
 
+    /**
+     * Test clicking on every menu item from the left slider menu and check the
+     * activity launched.
+     */
     public void testLeftSlideMenu() {
         Solo solo = this.getSolo();
 
         // open left slider
-        solo.setNavigationDrawer(Solo.OPENED);
+        solo.clickOnText("TopoSuite");
 
         solo.clickOnText("Home");
-        solo.setNavigationDrawer(Solo.OPENED);
+        solo.assertCurrentActivity("Home activity", MainActivity.class);
+        solo.clickOnText("TopoSuite");
+
         solo.clickOnText("Points Manager");
-        solo.setNavigationDrawer(Solo.OPENED);
+        solo.assertCurrentActivity("Points Manager activity", PointsManagerActivity.class);
+        solo.clickOnText("Points Manager");
+
+        solo.clickOnText("History");
+        solo.assertCurrentActivity("History activity", HistoryActivity.class);
+        solo.clickOnText("History");
+
+        solo.clickOnText("Jobs");
+        solo.assertCurrentActivity("Jobs activity", JobsActivity.class);
+        solo.clickOnText("Jobs");
+
+        solo.clickOnText("Settings");
+        solo.assertCurrentActivity("Settings activity", SettingsActivity.class);
+        solo.clickOnText("Settings");
+
+        solo.clickOnText("Help");
+        solo.assertCurrentActivity("Help activity", HelpActivity.class);
+        solo.clickOnText("Help");
     }
 }
