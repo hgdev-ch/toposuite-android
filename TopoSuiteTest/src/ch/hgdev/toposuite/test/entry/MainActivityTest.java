c9696c1b52518c1c0c933732a10d7d0420386728
Robin Hahling <robin.hahling@hgdev.ch>
2014-04-13 16:39:53 +0200

    create basic robotium test for the main activity

--- TopoSuiteTest/src/ch/hgdev/toposuite/test/entry/MainActivityTest.java
+++ TopoSuiteTest/src/ch/hgdev/toposuite/test/entry/MainActivityTest.java
@@ -3,9 +3,29 @@ package ch.hgdev.toposuite.test.entry;
 import ch.hgdev.toposuite.entry.MainActivity;
 import ch.hgdev.toposuite.test.testutils.TestActivity;
 
+import com.robotium.solo.Solo;
+
+/**
+ * This set of tests click on all the menus accessible from the main activity.
+ * 
+ * @author HGdev
+ * 
+ */
 public class MainActivityTest extends TestActivity<MainActivity> {
 
     public MainActivityTest() {
         super(MainActivity.class);
+    }
+
+    public void testLeftSlideMenu() {
+        Solo solo = this.getSolo();
+
+        // open left slider
+        solo.setNavigationDrawer(Solo.OPENED);
+
+        solo.clickOnText("Home");
+        solo.setNavigationDrawer(Solo.OPENED);
+        solo.clickOnText("Points Manager");
+        solo.setNavigationDrawer(Solo.OPENED);
     }
 }
