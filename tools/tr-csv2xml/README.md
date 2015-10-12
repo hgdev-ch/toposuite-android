0e1783f4601bb8653621fc448a3c1df7b6475986
Kevin Gillieron <kevin.gillieron@gw-computing.net>
2015-10-12 23:12:27 +0200

    tools: add script that converts CSV translations file to Android XML files

---
+++ tools/tr-csv2xml/README.md
@@ -0,0 +1,33 @@
+# tr-csv2xml
+
+tr-csv2xml converts a CSV file of translations into appropriate XML files.
+
+CSV file must have the following format:
+
+* First line contain columns labels
+* Column 1 contains the translations labels
+* Column 2 contains the english translation
+* Column 3 contains the french translation
+* Column 4 contains the german translation
+
+
+## Dependencies
+
+The only dependency is [Go](https://golang.org/)
+
+
+## Build
+
+To build this tool, issue the following command:
+
+```
+go build .
+```
+
+## Usage
+
+See
+
+```
+tr-csv2xml -h
+```
---
+++ tools/tr-csv2xml/main.go
@@ -0,0 +1,187 @@
+// Copyright 2015 The project AUTHORS. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// tr-csv2xml converts a CSV file of translations into appropriate XML files.
+//
+// CSV file must have the following format:
+//   * First line contain columns labels
+//   * Column 1 contains the translations labels
+//   * Column 2 contains the english translation
+//   * Column 3 contains the french translation
+//   * Column 4 contains the german translation
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"text/template"
+)
+
+// output filename
+const filename = "strings.xml"
+
+// Language code for non-default language
+const (
+	LANG_FR = "fr" // French
+	LANG_DE = "de" // German
+)
+
+const perm = 0755 // output directory permissions
+
+// template for rendering Android XML translation file
+const tmpl = `<?xml version="1.0" encoding="utf-8"?>
+<resources xmlns:xliff="urn:oasis:names:tc:xliff:document:1.2">
+	{{ range $index, $value := . }}<string name="{{ $value.Name }}">{{ $value.Value }}</string>
+	{{ end }}
+</resources>`
+
+// render templates
+func render(name string, w io.Writer, data []attr) error {
+	t, err := template.New(name).Parse(tmpl)
+	if err != nil {
+		return err
+	}
+	return t.Execute(w, data)
+}
+
+// attr represents a XML attribute.
+type attr struct {
+	Name  string
+	Value string
+}
+
+// fatal logs to stderr whatever is given as input and exit with status 1.
+func fatal(a ...interface{}) {
+	fmt.Fprintln(os.Stderr, a...)
+	os.Exit(1)
+}
+
+// same as fatal, but with formatting options
+func fatalf(format string, a ...interface{}) {
+	fatal(fmt.Sprintf(format, a...))
+}
+
+// print usage
+func usage() {
+	fmt.Fprintf(os.Stdout, "Usage: %s [CSV FILE]\n", os.Args[0])
+	flag.PrintDefaults()
+}
+
+// command line options
+var (
+	sep       = flag.String("s", ",", "CSV separator")
+	outputDir = flag.String("o", "values", "Output folder")
+)
+
+func main() {
+	flag.Usage = usage
+	flag.Parse()
+
+	if n := flag.NArg(); n != 1 {
+		fatalf("invalid # of arguments: expected 1, found %d", n)
+	}
+
+	if len(*sep) != 1 {
+		fatal("invalid separator")
+	}
+
+	if len(*outputDir) == 0 {
+		fatal("output directory cannot be empty")
+	}
+
+	// per-lang directories
+	var (
+		dirEn = *outputDir
+		dirFr = *outputDir + "-" + LANG_FR
+		dirDe = *outputDir + "-" + LANG_DE
+	)
+
+	// create output directories
+	if err := os.MkdirAll(dirEn, perm); err != nil {
+		fatal(err)
+	}
+	if err := os.MkdirAll(dirFr, perm); err != nil {
+		fatal(err)
+	}
+	if err := os.MkdirAll(dirDe, perm); err != nil {
+		fatal(err)
+	}
+
+	in, err := os.Open(flag.Arg(0))
+	if err != nil {
+		fatal(err)
+	}
+	defer in.Close()
+
+	r := csv.NewReader(in)
+	r.Comma = rune((*sep)[0])
+
+	// read all CSV at once, since we know it will fit in memory
+	records, err := r.ReadAll()
+	if err != nil {
+		fatal(err)
+	}
+
+	if len(records) <= 1 {
+		fatal("empty input file")
+	}
+
+	// total entries (first line is not taken into account)
+	numEntries := len(records) - 1
+
+	// create list of translations for each language
+	en := make([]attr, numEntries, numEntries)
+	fr := make([]attr, numEntries, numEntries)
+	de := make([]attr, numEntries, numEntries)
+
+	for idx, entry := range records[1:] {
+		if len(entry) != 4 {
+			fatalf("malformed CSV entry at line %d", idx+2)
+		}
+		en[idx] = attr{Name: entry[0], Value: entry[1]}
+		fr[idx] = attr{Name: entry[0], Value: entry[2]}
+		de[idx] = attr{Name: entry[0], Value: entry[3]}
+	}
+
+	// render template for each language
+
+	// render english translations
+	outputEn, err := os.Create(filepath.Join(dirEn, filename))
+	if err != nil {
+		fatal(err)
+	}
+	defer outputEn.Close()
+
+	if err := render(dirEn, outputEn, en); err != nil {
+		fatal(err)
+	}
+
+	// render french translations
+	outputFr, err := os.Create(filepath.Join(dirFr, filename))
+	if err != nil {
+		fatal(err)
+	}
+	defer outputFr.Close()
+
+	if err := render(dirFr, outputFr, fr); err != nil {
+		fatal(err)
+	}
+
+	// render german translations
+	outputDe, err := os.Create(filepath.Join(dirDe, filename))
+	if err != nil {
+		fatal(err)
+	}
+	defer outputDe.Close()
+
+	if err := render(dirDe, outputDe, de); err != nil {
+		fatal(err)
+	}
+
+	fmt.Println("Done.")
+}
